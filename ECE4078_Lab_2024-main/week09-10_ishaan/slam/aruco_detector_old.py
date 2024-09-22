f7c50f939b34efa859cf2c89e4cc71c3068f2387
lbos0002 <lbos0002@student.monash.edu>
2024-09-22 14:19:47 +1000

    ekf

--- ECE4078_Lab_2024-main/week09-10_ishaan/slam/aruco_detector_old.py
+++ ECE4078_Lab_2024-main/week09-10_ishaan/slam/aruco_detector_old.py
@@ -12,8 +12,8 @@ class aruco_detector:
         self.distortion_params = robot.camera_dist
 
         self.marker_length = marker_length
-        self.aruco_params = cv2.aruco.DetectorParameters() # updated to work with newer OpenCV
-        self.aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100) # updated to work with newer OpenCV
+        self.aruco_params = cv2.aruco.DetectorParameters_create()
+        self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
     
     def detect_marker_positions(self, img):
         # Perform detection
@@ -37,21 +37,9 @@ class aruco_detector:
             else:
                 seen_ids.append(idi)
 
-             # Get the rotation and translation vectors
-            rvec = rvecs[i]
-            tvec = tvecs[i]
-
-            # Convert rotation vector to rotation matrix
-            R, _ = cv2.Rodrigues(rvec)
-
-            # Calculate the offset from the marker to the cube's center
-            offset = np.array([0, 0, self.marker_length / 2]).reshape((3, 1))
-
-            # Transform the marker position to the cube's center
-            cube_center = tvec.T + R @ offset
-
-            # Convert to 2D coordinates (assuming the robot's pose is aligned with the principal axis)
-            lm_bff2d = np.block([[cube_center[2, 0]], [-cube_center[0, 0]]]).reshape(-1, 1)
+            lm_tvecs = tvecs[ids==idi].T
+            lm_bff2d = np.block([[lm_tvecs[2,:]],[-lm_tvecs[0,:]]])
+            lm_bff2d = np.mean(lm_bff2d, axis=1).reshape(-1,1)
 
             lm_measurement = measure.Marker(lm_bff2d, idi)
             measurements.append(lm_measurement)
